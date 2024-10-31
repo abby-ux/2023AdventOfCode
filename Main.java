39a4711c60e4f3b6b9a0d7fd7522cc62e1907ce5
Abby Reese <136013660+abby-ux@users.noreply.github.com>
2024-10-31 15:31:47 -0400

    day 2

--- Main.java
+++ Main.java
@@ -3,6 +3,8 @@ import all_days.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(One.getCalibrationValue("input.txt"));
+        // System.out.println(One.getCalibrationValue("input.txt"));
+        // System.out.println(Two.sumOfGames("input2.txt"));
+        System.out.println("power sun: " + Two.getSumPowerSets("input.txt"));
     }
 }
