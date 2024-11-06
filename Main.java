3365308cfbc51998127d38cd80258ac967c7bca7
Abby Reese <136013660+abby-ux@users.noreply.github.com>
2024-11-05 20:02:04 -0500

    day 3

--- Main.java
+++ Main.java
@@ -5,6 +5,8 @@ public class Main {
     public static void main(String[] args) {
         // System.out.println(One.getCalibrationValue("input.txt"));
         // System.out.println(Two.sumOfGames("input2.txt"));
-        System.out.println("power sun: " + Two.getSumPowerSets("input.txt"));
+        // System.out.println("power sum: " + Two.getSumPowerSets("input.txt"));
+        // System.out.println ("part sum: " + Three.getPartsSum("input.txt"));
+        System.out.println("gear ratio: " + Three.getGearRatio("input.txt"));
     }
 }
